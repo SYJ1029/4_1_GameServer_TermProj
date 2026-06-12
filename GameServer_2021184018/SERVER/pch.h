749693ba999c2ecd6f235952aa6bf38949346f28
SYJ1029 <abg0986@gmail.com>
2026-06-13 01:08:14 +0900

    변환을 수행할 서버 레퍼런스

---
+++ GameServer_2021184018/SERVER/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+// C++ standard library headers
+#include <iostream>
+#include <string>
+// Windows API headers
+#include <windows.h>
+// Other commonly used headers can be added here as needed
+
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used Windows headers
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
